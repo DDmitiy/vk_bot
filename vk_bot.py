2a87d3eeac7f44b07e6345c66b0b56dc95385719
dima <dudov.dm@gmail.com>
2018-10-12 23:15:51 +0300

    change type again

--- vk_bot.py
+++ vk_bot.py
@@ -30,8 +30,8 @@ def longpolling(upload_tool):
 				print(action)
 				print(urls)
 				pic_path = urls[action[5]][0]
-				vk_pic_url = upload_tool.photo(pic_path)[0]
-				api.messages.send(user_id=action[3], message=vk_pic_url['url'], v=api_version, attachment='photo{}_{}'.format(vk_pic_url['owner_id'], vk_pic_url['id']))
+				vk_pic_url = upload_tool.document(pic_path)[0]
+				api.messages.send(user_id=action[3], message=vk_pic_url['url'], v=api_version, attachment='doc{}_{}'.format(vk_pic_url['owner_id'], vk_pic_url['id']))
 				os.remove(pic_path)
 
 
